71e6d629e085ab49be2fe8e092379be0c30ecb7e
Christian Ramos <48297556+christivn@users.noreply.github.com>
2020-08-18 14:56:14 +0200

    Add files via upload

---
+++ fakeGen.py
@@ -0,0 +1,11 @@
+
+def ip():
+    import random
+    ip=""
+    for i in range(4):
+        num=random.randint(0, 255)
+        if(i!=3):
+            ip+=str(num)+"."
+        else:
+            ip+=str(num)
+    return ip
---
+++ run.py
@@ -0,0 +1,4 @@
+import fakeGen
+
+ip=fakeGen.ip()
+print("\033[32mIP GENERADA:\033[0m \033[35m"+ip+"\033[0m")
